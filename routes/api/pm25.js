ad7350af1649bc9e0278a71659e6e9421d743b2c
huliyou <liyoupie@gmail.com>
2016-02-03 16:50:56 +0800

    解决utf-8编码问题

--- routes/api/pm25.js
+++ routes/api/pm25.js
@@ -1,10 +1,11 @@
 var express = require('express');
 require('isomorphic-fetch');
+var utf8 = require('utf8');
 
 var router = express.Router();
 
 router.get('/', function(req, res){
-   fetch('http://www.pm25.in/api/querys/pm2_5.json?'+'token=5j1znBVAsnSf5xQyNQyq'+'&city='+req.query.city+'&avg='+req.query.avg+'&stations='+req.query.stations)
+   fetch('http://www.pm25.in/api/querys/pm2_5.json?'+'token=5j1znBVAsnSf5xQyNQyq'+'&city='+utf8.encode(req.query.city)+'&avg='+req.query.avg+'&stations='+req.query.stations)
    .then(function (response) {
       return response.json()
    })
--- routes/api/station_names.js
+++ routes/api/station_names.js
@@ -1,10 +1,11 @@
 var express = require('express');
 require('isomorphic-fetch');
+var utf8 = require('utf8');
 
 var router = express.Router();
 
 router.get('/', function(req, res) {
-   fetch('http://www.pm25.in/api/querys/station_names.json?token=5j1znBVAsnSf5xQyNQyq'+'&city='+req.query.city)
+   fetch('http://www.pm25.in/api/querys/station_names.json?token=5j1znBVAsnSf5xQyNQyq'+'&city='+utf8.encode(req.query.city))
    .then(function(response) {
       return response.json()
    })
